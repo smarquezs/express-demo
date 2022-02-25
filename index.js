25e99aa81470c82a26580e4ea944f2c6d3af83fb
Sergio Márquez <smarquezs@gmail.com>
2022-02-25 17:57:41 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const Joi = require('joi');
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+const courses = [
+  { id: 1, name: 'course1' },
+  { id: 2, name: 'course2' },
+  { id: 3, name: 'course3' },
+];
+
+app.get('/api/courses', (req, res) => res.send(courses));
+
+app.get('/api/courses/:id', (req, res) => {
+  const course = courses.find(course => course.id === parseInt(req.params.id));
+  if (!course) res.status(404).send('The course with the given ID was not found.')
+  res.send(course)
+});
+
+app.post('/api/courses', (req, res) => {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required()
+  });
+
+  const body = req.body;
+  const result = schema.validate(body);
+
+  if(result.error) {
+    const errorMessage = result.error.details.map((item) => item.message).join(';');
+    res.status(400).send(errorMessage);
+    return
+  }
+
+  const course = {
+    id: courses.length + 1,
+    name: body.name
+  }
+  courses.push(course);
+  res.send(course)
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Listening on port ${port}...`));
